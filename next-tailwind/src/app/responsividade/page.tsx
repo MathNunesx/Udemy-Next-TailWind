4017fab89fe4a369c44db8ea8e73b33368e29896
Matheus Nunes <matheusjobs04@gmail.com>
2025-11-11 23:45:54 -0300

    Responsividade

---
+++ next-tailwind/src/app/responsividade/page.tsx
@@ -0,0 +1,15 @@
+export default function Responsividade(){
+    return(
+        <div>
+            <h1 className="
+            bg-slate-700
+            md:bg-blue-500
+            lg:bg-purple-500
+            xl:bg-red-500" > 
+                
+            Responsividade
+                
+            </h1>
+        </div>
+    )
+}
