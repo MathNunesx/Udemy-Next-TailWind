4e717cd3c03a77c4cb8b9efadbc1aa43231e04d4
Matheus Nunes <matheusjobs04@gmail.com>
2025-11-17 22:53:42 -0300

    Tipografia

---
+++ next-tailwind/src/app/tipografia/page.tsx
@@ -0,0 +1,11 @@
+export default function Tipografia(){
+    return(
+        <div>
+            <h1 className="font-mono text-2xl text-center">Titulo 01</h1>
+            <h1 className="font-mono text-right">Titulo 02</h1>
+            <h1 className="font-mono font-light">Titulo 03</h1>
+            <h1 className="font-mono italic text-center">Titulo 04</h1>
+            <h1 className="font-mono, text-[20px] md:text-[40px] lg:text-[60px] text-center" >Titulo 05 Responsivo</h1>            
+        </div>
+    )
+}
