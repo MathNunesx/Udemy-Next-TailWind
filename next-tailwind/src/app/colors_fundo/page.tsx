63e75bbb3b3d03cedfe650b432a6be2d6accd20a
Matheus Nunes <matheusjobs04@gmail.com>
2025-11-17 23:39:48 -0300

    Cores e fundo

---
+++ next-tailwind/src/app/colors_fundo/page.tsx
@@ -0,0 +1,17 @@
+export default function Aplicacao(){
+    return(
+        <div className="bg-indigo-950/75">
+            <h1 className="text-indigo-400"> Colors </h1>
+            <h1 className="text-[#00a732]"> Colors em HEXA </h1>
+
+            <button className="text-emerald-300 bg-stone-800 hover:bg-teal-800"> Hover ME</button>
+            {/* <br />
+            <button className="bg-[#5c55b6] hover:text-[#00a732]"> Hover ME</button> */}
+
+                <h1 className="h-600 bg-[url(/img/pexels-jdm-kuruma-1304609967-30145519.jpg)] bg-bottom"> Imagem de Fundo </h1>
+
+            
+
+        </div>
+    )
+}
