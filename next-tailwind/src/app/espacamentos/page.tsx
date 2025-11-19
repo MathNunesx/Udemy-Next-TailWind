e9bf5a65d189c59161ed983591395de9852110de
Matheus Nunes <matheusjobs04@gmail.com>
2025-11-18 23:21:48 -0300

    Padding e margin

---
+++ next-tailwind/src/app/espacamentos/page.tsx
@@ -0,0 +1,10 @@
+export default function Aplicacao2(){
+    return (
+        <div> 
+            <div className="bg-[#075bbb] p-5"> Padding</div>
+
+            <div className="bg-[#b14141] p-5 m-[60px]"> Margin </div>
+
+        </div>
+    )
+}
