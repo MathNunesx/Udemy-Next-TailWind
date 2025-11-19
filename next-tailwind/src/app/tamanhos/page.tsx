ef6641c5d59e5efdee2bea164a3bd29478d5c13c
Matheus Nunes <matheusjobs04@gmail.com>
2025-11-18 23:42:51 -0300

    tamanho e altura

---
+++ next-tailwind/src/app/tamanhos/page.tsx
@@ -0,0 +1,20 @@
+export default function AplicacaoLargura(){
+    return (
+    <div className="">
+        <div className="bg-[#084939] w-96 p-2"> 
+           
+           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus nisi, esse repellat pariatur similique veritatis quis alias corrupti minima quibusdam?
+           
+        </div>
+
+        <div className="bg-[#3a0849] w-96 p-2"> 
+           
+           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus nisi, esse repellat pariatur similique veritatis quis alias corrupti minima quibusdam?-2
+           
+        </div>
+
+        <div className="bg-[#5a133f] size-40 text-center"> Tamanho </div>
+    </div>
+        
+    )
+}
