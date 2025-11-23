2f5d8d7d6868050177113a1f25b787fab17503cf
Matheus Nunes <matheusjobs04@gmail.com>
2025-11-23 02:59:00 -0300

    Components

--- next-tailwind/src/app/bordes/page.tsx
+++ next-tailwind/src/app/bordes/page.tsx
@@ -1,7 +1,7 @@
 export default function AplicacaoBorder(){
     return (
     <div className="">
-        <div className="m-8 p-4 border-2 border-cyan-900 rounded-[12px]">
+        <div className="m-8 p-4 border-2 border-cyan-900 rounded-xl">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit, ad!
         </div>
     </div>
---
+++ next-tailwind/src/app/componentes/page.tsx
@@ -0,0 +1,11 @@
+export default function Componente(){
+    return(
+    <div className="p-5 m-3.5">
+        <button className="btn-sucesso"> Sucesso </button>
+
+        <button className="btn-alerta"> Alerta </button>
+
+        <button className="btn-erro"> Error </button>
+    </div>
+    )
+}
