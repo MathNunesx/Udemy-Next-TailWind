cd6c11b4f2f96e85b5749beac436751771ce6c17
Matheus Nunes <matheusjobs04@gmail.com>
2025-11-24 22:38:04 -0300

    Columns

---
+++ next-tailwind/src/app/columns/page.tsx
@@ -0,0 +1,12 @@
+export default function AplicacaoColumn(){
+    return (
+        <div className="p-5 columns-3 md:columns-2 lg:columns-1">
+            <div className="w-32 h-32 border p-2 bg-blue-500"> item 1</div>
+            <div className="w-32 h-32 border p-2 bg-blue-500"> item 2</div>
+            <div className="w-32 h-32 border p-2 bg-blue-500"> item 3</div>
+            <div className="w-32 h-32 border p-2 bg-blue-500"> item 4</div>
+            <div className="w-32 h-32 border p-2 bg-blue-500"> item 5</div>
+            <div className="w-32 h-32 border p-2 bg-blue-500"> item 6</div>
+        </div>
+    )
+}
