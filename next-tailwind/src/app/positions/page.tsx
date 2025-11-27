05e171d547e33d7da0df2db24cdc77082a229e54
Matheus Nunes <matheusjobs04@gmail.com>
2025-11-26 23:09:09 -0300

    Relative position

---
+++ next-tailwind/src/app/positions/page.tsx
@@ -0,0 +1,16 @@
+
+export default function AplicacaoFloat(){
+    return (
+        <div className="border p-10">
+            
+
+            <p className="bg-indigo-600 p-2 m-2"> DESTAQUE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, hic.</p>
+
+            <p className="bg-teal-700 p-2 m-2">  ITEM 2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, hic.</p>
+
+            <p className="bg-fuchsia-600 p-2 m-2 relative top-2">  ITEM 2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, hic.</p>
+
+            
+        </div>
+    )
+}
