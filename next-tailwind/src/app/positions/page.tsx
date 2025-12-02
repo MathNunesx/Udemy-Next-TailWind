8f474974d0c060f55d8149a2ac0977f3f22b5387
Matheus Nunes <matheusjobs04@gmail.com>
2025-12-01 23:20:48 -0300

    position absolute fixed e sticky

--- next-tailwind/src/app/positions/page.tsx
+++ next-tailwind/src/app/positions/page.tsx
@@ -4,11 +4,13 @@ export default function AplicacaoFloat(){
         <div className="border p-10">
             
 
-            <p className="bg-indigo-600 p-2 m-2"> DESTAQUE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, hic.</p>
+            <p className="bg-indigo-600 p-2 m-2 top-4 sticky"> DESTAQUE Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, hic.</p>
 
-            <p className="bg-teal-700 p-2 m-2">  ITEM 2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, hic.</p>
+            <p className="bg-teal-700 p-2 m-2 fixed"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos illum eligendi ipsa, sequi similique labore facere adipisci quas itaque quidem, minima tenetur ipsum sint nihil fugiat quam, rem alias eaque. Delectus amet quo commodi quod hic. Ducimus nulla quia necessitatibus. Totam, modi numquam mollitia expedita deleniti nihil aliquid cum dolor.</p>
+{/* 
+            <p className="bg-fuchsia-600 p-2 m-2 absolute bottom-2">  ITEM 2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, hic.</p> */}
 
-            <p className="bg-fuchsia-600 p-2 m-2 relative top-2">  ITEM 2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, hic.</p>
+            {/* <p className="bg-fuchsia-600 p-2 m-2 relative top-2">  ITEM 2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium, hic.</p> */}
 
             
         </div>
