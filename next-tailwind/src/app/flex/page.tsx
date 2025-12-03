87564aeac59b4357dbb6d43b5b73e2e2bca2c17e
Matheus Nunes <matheusjobs04@gmail.com>
2025-12-02 23:00:19 -0300

    Flex

---
+++ next-tailwind/src/app/flex/page.tsx
@@ -0,0 +1,12 @@
+
+export default function AplicacaoFlex(){
+    return (
+        <div className="border p-3 flex flex-row gap-4">
+            <div className="caixa basis-64"> Caixa 1</div>
+            <div className="caixa basis-96"> Caixa 2</div>
+            <div className="caixa basis-64"> Caixa 3</div>        
+ 
+            
+        </div>
+    )
+}
