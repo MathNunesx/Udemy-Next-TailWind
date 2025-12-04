685b9d92ee1490bb016d3ae898168e6fb9645873
Matheus Nunes <matheusjobs04@gmail.com>
2025-12-03 22:50:41 -0300

    ordem | crescimento | alinhamento

--- next-tailwind/src/app/flex/page.tsx
+++ next-tailwind/src/app/flex/page.tsx
@@ -2,9 +2,10 @@
 export default function AplicacaoFlex(){
     return (
         <div className="border p-3 flex flex-row gap-4">
-            <div className="caixa basis-64"> Caixa 1</div>
-            <div className="caixa basis-96"> Caixa 2</div>
-            <div className="caixa basis-64"> Caixa 3</div>        
+            {/* justify - grow - basis - order - align itens */}
+            <div className="caixa basis-48 order-1"> Caixa 1</div>
+            <div className="caixa grow"> Caixa 2</div>
+            <div className="caixa order-2"> Caixa 3</div>        
  
             
         </div>
