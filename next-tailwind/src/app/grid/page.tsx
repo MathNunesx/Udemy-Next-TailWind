45ec4aa50e14a7cb73faa341b315e6dff738f5e1
Matheus Nunes <matheusjobs04@gmail.com>
2025-12-06 18:57:55 -0300

    grid responsive

--- next-tailwind/src/app/grid/page.tsx
+++ next-tailwind/src/app/grid/page.tsx
@@ -1,9 +1,11 @@
 
 export default function aplicacaoGrid(){
     return (
-        <div className="border-4 p-3 grid grid-cols-3 gap-2">
+        // <div className="border-4 p-3 grid grid-cols-3 gap-2">
+        <div className="border-4 p-3 grid grid-cols-1 gap-2 md:grid grid-cols-2 lg:grid grid-cols-4">
          
-         <div className="caixa col-start-1 col-end-3">Caixa 1</div>
+         {/* <div className="caixa col-start-1 col-end-3">Caixa 1</div> */}
+         <div className="caixa col-span-1 md:col-span-2 lg:col-span-4">Caixa 1</div>
          <div className="caixa">Caixa 2</div>
          <div className="caixa">Caixa 3</div>
          <div className="caixa">Caixa 4</div>
@@ -11,7 +13,8 @@ export default function aplicacaoGrid(){
          <div className="caixa">Caixa 6</div>
          <div className="caixa">Caixa 7</div>
          <div className="caixa">Caixa 8</div>
-         <div className="caixa col-span-2">Caixa 9</div>
+         {/* <div className="caixa col-span-2">Caixa 9</div> */}
+         <div className="caixa">Caixa 9</div>
 
             
         </div>
