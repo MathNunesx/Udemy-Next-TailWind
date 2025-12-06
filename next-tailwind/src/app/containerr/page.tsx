ee976e1337b329b3784fb8996d825faf4ff35026
Matheus Nunes <matheusjobs04@gmail.com>
2025-12-06 19:33:32 -0300

    container

---
+++ next-tailwind/src/app/containerr/page.tsx
@@ -0,0 +1,17 @@
+
+export default function aplicacaoContainer(){
+    return (
+       
+        <div className="container mx-auto bg-[#22143b85] border-2">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam nulla maiores mollitia? Dignissimos incidunt mollitia distinctio dicta illo reiciendis dolore culpa quo obcaecati possimus? Officia animi odio qui officiis rem.</p>
+            
+            <div className="border p-3 grid gap-2 grid-cols-4">
+                <div className="caixa">Caixa 1</div>
+                <div className="caixa">Caixa 2</div>
+                <div className="caixa">Caixa 3</div>
+            </div>
+
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias eligendi, aliquid veniam pariatur veritatis optio quis deserunt molestiae. Nobis doloribus suscipit, nesciunt earum ad aliquam praesentium sed mollitia soluta fugiat.</p>
+        </div>
+    )
+}
